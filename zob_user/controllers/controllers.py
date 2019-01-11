8f93b323c039cda63a6e23af7356e63449a7f01c
winboy99 <odooht@163.com>
2019-01-11 11:15:54 +0800

    login update

--- zob_user/controllers/controllers.py
+++ zob_user/controllers/controllers.py
@@ -75,7 +75,8 @@ class JsonApi(http.Controller):
 
     @http.route('/json/api',type='json', auth='user',cors='*',csrf=False)
     def json_api(self,model,method, args,kwargs):
-        print('json/api:',model,method, args,kwargs)
+        _logger.info('model %s call %s with %s, %s', model,method, args,kwargs)
+
         if method not in ['read2','search_read2']:
             return api.call_kw(request.env[model],method,args,kwargs)
 
@@ -86,6 +87,32 @@ class JsonApi(http.Controller):
         }[method](request.env[model], *args, **kwargs)
 
         return ret
+
+    @http.route('/json/user/login',type='json', auth='none', cors='*', csrf=False )
+    def login(self,db,login,password, **kw):
+        _logger.info('call login with %s, %s, %s', db,login,password )
+
+        uid = http.request.env['res.users'].authenticate(
+                     db,login,password,None )
+
+        session = request.session
+
+        session.db = db
+        session.uid = uid
+        session.login = login
+        session.session_token = uid and security.compute_session_token(session, request.env)
+
+        request.uid = uid
+        request.disable_db = False
+
+        if uid:
+            session.context = http.request.env['res.users'].context_get() or {}
+            session.context['uid'] = uid
+            session._fix_lang(session.context)
+
+        http.root.session_store.save(session)
+        return request.env['ir.http'].session_info()
+
 
     @http.route('/json/user/register',type='json', auth='none', cors='*', csrf=False )
     def register(self,db,login,password):
@@ -98,54 +125,5 @@ class JsonApi(http.Controller):
         with registry(db).cursor() as cr:
             env = api.Environment(cr, SUPERUSER_ID, {})
             return env['res.users'].reset_new_password(login,password)
-
-    def _login_return(self,status,type,authority, userinfo=None):
-        res = {
-            'status': status,
-            'type':  type,
-            'currentAuthority': authority }
-
-        if not userinfo:
-            return res
-
-        res.update(userinfo)
-        print(res)
-        return res
-
-
-    @http.route('/json/user/login',type='json', auth='none', cors='*', csrf=False )
-    def login(self,db,login,password, type):
-        print('1231',db,login,password, type)
-
-        if type not in ['account', 'mobile']:
-            return self._login_return('error', type, 'guest')
-
-        if type in ['mobile']:
-            return self._login_return('error', type, 'guest')
-
-        uid = http.request.env['res.users'].authenticate(
-                     db,login,password,None )
-
-        if not uid:
-            return self._login_return('error', type, 'guest')
-
-        session = http.request.session
-        session.db = db
-        session.uid = uid
-        session.login = login
-        session.session_token = uid and security.compute_session_token(session)
-        session.context = http.request.env['res.users'].context_get() or {}
-        session.context['uid'] = uid
-        session._fix_lang(session.context)
-        http.root.session_store.save(session)
-
-        authority = 'admin'  #  from userinfo
-        userinfo = {
-                 'sid': session.sid,
-                 'uid': uid,
-                 'name':str( uid ) + 'name'
-                 }  # user info
-
-        return self._login_return('ok', type, authority, userinfo)
 
 
